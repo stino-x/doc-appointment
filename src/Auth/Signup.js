342937a384a7eddbf373abfd74f40674d60d9b10
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-03-01 22:23:49 +0200

    Finish setting up the project

--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div class='page'>
+          <div>Signup</div>
+    <form class='signup'>
+      <input type='email'/>
+      <input type='password'/>
+    </form>
+    </div>
   );
 }
