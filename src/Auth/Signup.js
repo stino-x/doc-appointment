92b57ac6268eec35dba5091c7c60ff01d6b7c1b6
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-03-01 22:46:10 +0200

    Fix linters

--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -3,11 +3,7 @@ import React from 'react';
 export default function Signup() {
   return (
     <div class='page'>
-          <div>Signup</div>
-    <form class='signup'>
-      <input type='email'/>
-      <input type='password'/>
-    </form>
+      <div>Signup</div>
     </div>
   );
 }
